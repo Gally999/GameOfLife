65117419b7469c9bc22d1cdb079082f660735ba5
Cécile Daguin <cecile.daguin@thalesdigital.io>
2020-08-13 09:14:10 +0200

    cell component to functional

--- src/components/GridDisplay/CellComponent.js
+++ src/components/GridDisplay/CellComponent.js
@@ -1,29 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './CellComponent.css';
 import CellModel from '../../models/CellModel';
 
-class CellComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.switchState = this.switchState.bind(this);
-  }
-
-  switchState() {
-    const { cell, onCellClicked } = this.props;
-    onCellClicked(cell);
-  }
-
-  render() {
-    const { cell } = this.props;
-    return (
-      <td
-        className={cell.isAlive ? 'alive' : 'dead'} 
-        onClick={this.switchState} 
-      ></td>
-    );
-  }
-}
+const CellComponent = ({ cell, onCellClicked }) => (
+  <td
+    className={cell.isAlive ? 'alive' : 'dead'} 
+    onClick={() => onCellClicked(cell)} 
+  ></td>
+)
 
 CellComponent.propTypes = {
   cell: CellModel.shape.isRequired,
